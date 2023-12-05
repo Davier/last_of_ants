8a90bb847dfb779e9b58d9dfc61dfc4b003ce723
Brice DAVIER <bricedavier@gmail.com>
2023-12-06 00:44:20 +0100

    Improve jumping

--- src/bin/character_control.rs
+++ src/bin/character_control.rs
@@ -5,13 +5,12 @@ use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_rapier2d::prelude::*;
 use last_of_ants::{
     components::{
-        entities::{update_player_sensor, AntBundle, Player},
-        nav_mesh::{debug_nav_mesh, NavNode},
+        entities::{update_player_sensor, Player},
+        nav_mesh::debug_nav_mesh,
     },
     helpers::{toggle_on_key, toggle_physics_debug},
     GamePlugin,
 };
-use rand::seq::IteratorRandom;
 
 fn main() {
     App::new()
@@ -61,88 +60,114 @@ fn player_movement(
     time: Res<Time>,
     mut last_time_on_left_wall: Local<f32>,
     mut last_time_on_right_wall: Local<f32>,
+    mut last_time_on_ground: Local<f32>,
+    mut jump_available: Local<bool>,
 ) {
-    let walk_speed = 160.;
-    let fly_speed = 10.;
+    let Ok((mut controller, mut velocity, player, controller_output)) = players.get_single_mut()
+    else {
+        return;
+    };
+
+    let mut walk_speed = 160.;
     let gravity = -9.81 * 10.0 * 16.; // FIXME: scale
     let dt = time.delta_seconds();
     let jump_impulse = 8. / dt;
     let max_sliding_velocity = 50.;
-    let jump_tolerance = 0.05; // [s]
+    let jump_tolerance = 0.1; // [s]
 
-    for (mut controller, mut velocity, player, controller_output) in &mut players {
-        let is_on_ground = !player.on_ground.is_empty();
-        let is_on_wall = !player.on_wall.is_empty();
-        if player.is_on_left_wall {
-            *last_time_on_left_wall = time.elapsed_seconds();
-        }
-        let is_on_left_wall_recently =
-            time.elapsed_seconds() - *last_time_on_left_wall < jump_tolerance;
-        if player.is_on_right_wall {
-            *last_time_on_right_wall = time.elapsed_seconds();
-        }
-        let is_on_right_wall_recently =
-            time.elapsed_seconds() - *last_time_on_right_wall < jump_tolerance;
-        let is_on_ceiling = controller_output
-            .map(|output| output.effective_translation.y < output.desired_translation.y)
-            .unwrap_or(false);
-
-        let space_pressed = inputs.pressed(KeyCode::Space);
-        let left_pressed = inputs.pressed(KeyCode::A);
-        let right_pressed = inputs.pressed(KeyCode::D);
-        let up_pressed = inputs.pressed(KeyCode::W);
-        let down_pressed = inputs.pressed(KeyCode::S);
-
-        let v = &mut velocity.linvel;
-        // info!("{:5?} | {:?}", is_on_ground, is_on_wall);
-
-        // Reset velocity
-        if is_on_ground {
-            v.x = 0.;
-            v.y = 0.;
-        }
-        if is_on_ceiling {
-            v.y = 0.;
-        }
-        if player.is_on_left_wall && v.x < 0. {
-            v.x = 0.;
-        }
-        if player.is_on_right_wall && v.x > 0. {
-            v.x = 0.;
-        }
-
-        // Apply gravity
-        v.y += gravity * dt;
-        if is_on_wall && !down_pressed && v.y < -max_sliding_velocity {
-            // info!("Sliding");
-            v.y = -max_sliding_velocity;
-        }
-
-        // Move from inputs
-        if right_pressed && (is_on_ground || player.is_on_left_wall) {
-            v.x += walk_speed;
-        };
-        if left_pressed && (is_on_ground || player.is_on_right_wall) {
-            v.x -= walk_speed;
-        };
-        if space_pressed {
-            if is_on_ground {
-                v.y = jump_impulse;
-            } else if is_on_left_wall_recently && right_pressed {
-                v.y = jump_impulse;
-                v.x = jump_impulse / 3.;
-                // info!("Wall jump left");
-            } else if is_on_right_wall_recently && left_pressed {
-                v.y = jump_impulse;
-                v.x = -jump_impulse / 3.;
-                // info!("Wall jump right");
-            }
-        }
-
-        let delta_position = *v * dt;
-        // dbg!(&delta_position);
-        controller.translation = Some(delta_position);
+    let is_on_ground = !player.on_ground.is_empty();
+    let is_on_wall = !player.on_wall.is_empty();
+    if player.is_on_left_wall {
+        *last_time_on_left_wall = time.elapsed_seconds();
     }
+    let is_on_left_wall_recently =
+        time.elapsed_seconds() - *last_time_on_left_wall < jump_tolerance;
+    if player.is_on_right_wall {
+        *last_time_on_right_wall = time.elapsed_seconds();
+    }
+    let is_on_right_wall_recently =
+        time.elapsed_seconds() - *last_time_on_right_wall < jump_tolerance;
+    if is_on_ground {
+        *last_time_on_ground = time.elapsed_seconds();
+    }
+    let is_on_ground_recently = time.elapsed_seconds() - *last_time_on_ground < jump_tolerance;
+    let is_on_ceiling = controller_output
+        .map(|output| output.effective_translation.y < output.desired_translation.y)
+        .unwrap_or(false);
+
+    let space_pressed = inputs.pressed(KeyCode::Space);
+    let left_pressed = inputs.pressed(KeyCode::A);
+    let right_pressed = inputs.pressed(KeyCode::D);
+    let up_pressed = inputs.pressed(KeyCode::W);
+    let down_pressed = inputs.pressed(KeyCode::S);
+    let shift_pressed = inputs.pressed(KeyCode::ShiftLeft);
+
+    let v = &mut velocity.linvel;
+    // info!("{:5?} | {:?}", is_on_ground, is_on_wall);
+
+    // Slow down if shift is pressed
+    if shift_pressed {
+        walk_speed *= 0.5;
+    }
+
+    // Only allow one jump per pressed key
+    if inputs.just_pressed(KeyCode::Space) {
+        *jump_available = true;
+    }
+
+    // Reset velocity
+    if is_on_ground {
+        v.x = 0.;
+        v.y = 0.;
+    }
+    if is_on_ceiling {
+        v.y = 0.;
+    }
+    if player.is_on_left_wall && v.x < 0. {
+        v.x = 0.;
+    }
+    if player.is_on_right_wall && v.x > 0. {
+        v.x = 0.;
+    }
+
+    // Apply gravity
+    v.y += gravity * dt;
+    if is_on_wall && !down_pressed && v.y < -max_sliding_velocity {
+        // info!("Sliding");
+        v.y = -max_sliding_velocity;
+    }
+
+    // Move from inputs
+    if right_pressed
+        && (is_on_ground_recently || is_on_left_wall_recently || is_on_right_wall_recently)
+    {
+        v.x = walk_speed;
+    };
+    if left_pressed
+        && (is_on_ground_recently || is_on_left_wall_recently || is_on_right_wall_recently)
+    {
+        v.x = -walk_speed;
+    };
+    if space_pressed && *jump_available {
+        if is_on_ground {
+            v.y = jump_impulse;
+            *jump_available = false;
+        } else if is_on_left_wall_recently && right_pressed {
+            v.y = jump_impulse;
+            v.x = jump_impulse / 3.;
+            *jump_available = false;
+            // info!("Wall jump left");
+        } else if is_on_right_wall_recently && left_pressed {
+            v.y = jump_impulse;
+            v.x = -jump_impulse / 3.;
+            *jump_available = false;
+            // info!("Wall jump right");
+        }
+    }
+
+    let delta_position = *v * dt;
+    // dbg!(&delta_position);
+    controller.translation = Some(delta_position);
 }
 
 fn attach_camera_to_player(
